c9ab28965b548645c1a66de45c92b10a14adb3aa
King-Sathaporn <101379107+King-Sathaporn@users.noreply.github.com>
2022-07-06 22:18:41 +0700

    6/7/2022

---
+++ controls.js
@@ -0,0 +1,52 @@
+class Controls{
+    constructor(type){
+        this.forward = false;
+        this.backward = false;
+        this.left = false;
+        this.right = false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward = true;
+                break
+        }
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown = (e) => {
+            switch(e.key){
+                case 'w':
+                    this.forward = true;
+                    break;
+                case 's':
+                    this.backward = true;
+                    break;
+                case 'a':
+                    this.left = true;
+                    break;
+                case 'd':
+                    this.right = true;
+                    break;
+            }
+        }
+        document.onkeyup = (e) => {
+            switch(e.key){
+                case 'w':
+                    this.forward = false;
+                    break;
+                case 's':
+                    this.backward = false;
+                    break;
+                case 'a':
+                    this.left = false;
+                    break;
+                case 'd':
+                    this.right = false;
+                    break;
+            }
+        }
+    }
+}
